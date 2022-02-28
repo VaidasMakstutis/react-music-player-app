68dd4b288b7ad4f3e2cbbfbe84268b79cb1bbc23
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-02-28 20:18:41 +0200

    UseState for data, useEffect for songs changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import {useState, useEffect} from "react";
+import Player from "./Components/Player";
 import songs from "./Data/songs";
 
 const App = () => {
 
-useState([songs]);
+  useState([songs]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex])
 
   return (
     <div className="App">
-      Initial commit!
+      <Player 
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
