490243128c2797ab8dbeaf22b76c5e3b00a344b3
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-03-12 20:02:32 +0200

    Showed songs list in player

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,58 +1,79 @@
 import React, { useState, useEffect, useRef } from "react";
 import PlayerDetails from "./PlayerDetails";
 import PlayerControls from "./PlayerControls";
-import PlayerSongsList from "./PlayerSongsList";
+import songs from "../Data/songs";
 
-const Player = (props) => {
-
+const Player = props => {
   const audioElement = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  
+
   useEffect(() => {
-      if (isPlaying) {
-          audioElement.current.play();
-      } else {
-          audioElement.current.pause();
-      }
-  });
-  
+    if (isPlaying) {
+      audioElement.current.play();
+      console.log("Duration: ", (audioElement.current.duration / 60).toFixed(), (audioElement.current.duration % 60).toFixed());
+    } else {
+      audioElement.current.pause();
+    }
+  }, [isPlaying]);
+
+  const stopSong = () => {
+    audioElement.current.pause();
+    audioElement.current.currentTime = 0;
+  };
+
   const skipSong = (forwards = true) => {
-      if (forwards) {
-          props.setCurrentSongIndex(() => {
-              let temp = props.currentSongIndex;
-              temp++;
-  
-              if (temp > props.songs.length - 1) {
-                  temp = 0;
-              }
-              return temp;
-          });
-      } else {
-          props.setCurrentSongIndex(() => {
-              let temp = props.currentSongIndex;
-              temp--;
-  
-              if (temp < 0) {
-                  temp = props.songs.length - 1;
-              }
-              return temp;
-          });
-      }
-  }
-  
-      return (
-          <div className="player">
-              <audio src={props.songs[props.currentSongIndex].src} ref={audioElement}></audio>
-              <h3>Playing now</h3>
-              <PlayerDetails song={props.songs[props.currentSongIndex]} />
-              <PlayerControls 
-              isPlaying={isPlaying} 
-              setIsPlaying={setIsPlaying} 
-              skipSong={skipSong} 
-              />
-              <PlayerSongsList />
-          </div>
-      )
-  }
+    if (forwards) {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp++;
+
+        if (temp > props.songs.length - 1) {
+          temp = 0;
+        }
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+        return temp;
+      });
+    }
+  };
+
+  const clickHandler = id => {
+    props.setCurrentSongIndex(id - 1);
+    console.log(id);
+  };
+
+  const doubleClickHandler = () => {
+    stopSong();
+    audioElement.current.play();
+  };
+
+  return (
+    <div className="player">
+      <audio src={props.songs[props.currentSongIndex].src} ref={audioElement}></audio>
+      <h3>Playing now</h3>
+      <PlayerDetails song={props.songs[props.currentSongIndex]} />
+      <PlayerControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} skipSong={skipSong} stopSong={stopSong} />
+      
+      <div className="container-songs-list">
+        <select multiple size={songs.length}>
+          {songs.map((item, key) => (
+            <option key={item.id} onClick={() => clickHandler(item.id)} onDoubleClick={() => doubleClickHandler()}>
+              {item.id}.{item.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      
+    </div>
+  );
+};
 
 export default Player;
--- src/Data/songs.js
+++ src/Data/songs.js
@@ -1,67 +1,67 @@
 const songs = [
-    {
-      id: 1,
-      title: "Tocas Miracle (Inpetto Remix)",
-      artist: "Fragma",
-      img_src: "./images/01-fragma.jpg",
-      src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
-    },
-    {
-      id: 2,
-      title: "Celebration",
-      artist: "Madonna",
-      img_src: "./images/02-madonna.jpg",
-      src: "./music/02-madonna-celebration.mp3"
-    },
-    {
-      id: 3,
-      title: "King of disco",
-      artist: "Akcent",
-      img_src: "./images/03-akcent.jpg",
-      src: "./music/03-akcent - King of disco.mp3"
-    },
-    {
-      id: 4,
-      title: "Tocas Miracle (Inpetto Remix)",
-      artist: "Fragma",
-      img_src: "./images/01-fragma.jpg",
-      src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
-    },
-    {
-      id: 5,
-      title: "Celebration",
-      artist: "Madonna",
-      img_src: "./images/02-madonna.jpg",
-      src: "./music/02-madonna-celebration.mp3"
-    },
-    {
-      id: 6,
-      title: "King of disco",
-      artist: "Akcent",
-      img_src: "./images/03-akcent.jpg",
-      src: "./music/03-akcent - King of disco.mp3"
-    },
-    {
-      id: 7,
-      title: "Tocas Miracle (Inpetto Remix)",
-      artist: "Fragma",
-      img_src: "./images/01-fragma.jpg",
-      src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
-    },
-    {
-      id: 8,
-      title: "Celebration",
-      artist: "Madonna",
-      img_src: "./images/02-madonna.jpg",
-      src: "./music/02-madonna-celebration.mp3"
-    },
-    {
-      id: 9,
-      title: "King of disco",
-      artist: "Akcent",
-      img_src: "./images/03-akcent.jpg",
-      src: "./music/03-akcent - King of disco.mp3"
-    },
-  ]
+  {
+    id: 1,
+    title: "Tocas Miracle (Inpetto Remix)",
+    artist: "Fragma",
+    img_src: "./images/01-fragma.jpg",
+    src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
+  },
+  {
+    id: 2,
+    title: "Celebration",
+    artist: "Madonna",
+    img_src: "./images/02-madonna.jpg",
+    src: "./music/02-madonna-celebration.mp3"
+  },
+  {
+    id: 3,
+    title: "King of disco",
+    artist: "Akcent",
+    img_src: "./images/03-akcent.jpg",
+    src: "./music/03-akcent - King of disco.mp3"
+  },
+  {
+    id: 4,
+    title: "Tocas Miracle (Inpetto Remix)",
+    artist: "Fragma",
+    img_src: "./images/01-fragma.jpg",
+    src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
+  },
+  {
+    id: 5,
+    title: "Celebration",
+    artist: "Madonna",
+    img_src: "./images/02-madonna.jpg",
+    src: "./music/02-madonna-celebration.mp3"
+  },
+  {
+    id: 6,
+    title: "King of disco",
+    artist: "Akcent",
+    img_src: "./images/03-akcent.jpg",
+    src: "./music/03-akcent - King of disco.mp3"
+  },
+  {
+    id: 7,
+    title: "Tocas Miracle (Inpetto Remix)",
+    artist: "Fragma",
+    img_src: "./images/01-fragma.jpg",
+    src: "./music/01-fragma_-_tocas_miracle_(inpetto_remix).mp3"
+  },
+  {
+    id: 8,
+    title: "Celebration",
+    artist: "Madonna",
+    img_src: "./images/02-madonna.jpg",
+    src: "./music/02-madonna-celebration.mp3"
+  },
+  {
+    id: 9,
+    title: "King of disco",
+    artist: "Akcent",
+    img_src: "./images/03-akcent.jpg",
+    src: "./music/03-akcent - King of disco.mp3"
+  },
+]
 
-  export default songs;
+export default songs;
