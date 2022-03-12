ed7c8da431b1a024f63b6cf28e757fda3720854d
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-03-12 20:03:50 +0200

    Stop button in control component

--- src/Components/PlayerControls.jsx
+++ src/Components/PlayerControls.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faForward, faBackward} from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faPause, faStop, faForward, faBackward} from "@fortawesome/free-solid-svg-icons";
 
 const PlayerControls = (props) => {
     return (
         <div className="player-controls">
             <button className="skip-btn" onClick={() => props.skipSong(false)}>
-                <FontAwesomeIcon icon={faBackward} />
+                <FontAwesomeIcon icon={faBackward} title="Previous song" />
             </button>
             <button className="play-btn" onClick={() => props.setIsPlaying(!props.isPlaying)}>
-                <FontAwesomeIcon icon={props.isPlaying ? faPause: faPlay} />
+                <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} title="Play/Pause" />
+            </button>
+            <button className="stop-btn" onClick={props.stopSong}>
+                <FontAwesomeIcon icon={faStop} title="Stop" />
             </button>
             <button className="skip-btn" onClick={() => props.skipSong(false)}>
-                <FontAwesomeIcon icon={faForward} />
+                <FontAwesomeIcon icon={faForward} title="Next song" />
             </button>
         </div>
     )
