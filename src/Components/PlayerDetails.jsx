0cfbd9d91dcc2fba9362d63a87b8f8cd4caa8569
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-03-13 20:44:55 +0200

    Title for img src in PlayerDetails.jsx

--- src/Components/PlayerDetails.jsx
+++ src/Components/PlayerDetails.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const PlayerDetails = (props) => {
+
   return (
     <div className="player-details">
       <div className="details-image">
-        <img src={props.song.img_src} alt="" />
+        <img src={props.song.img_src} title={props.song.artist} alt={props.song.artist} />
       </div>
       <h3 className="details-title">{props.song.title}</h3>
       <h4 className="details-artist">{props.song.artist}</h4>
